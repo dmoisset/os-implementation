96ad1061dea27e248e82d5dfca6f90b5caad3598
Nicolas Wolovick <nwolovick@gmail.com>
2011-04-29 18:21:39 -0300

    Backportee a mano del project1, no me sale el scripts/startProject

--- src/project2/src/geekos/elf.c
+++ src/project2/src/geekos/elf.c
@@ -15,7 +15,6 @@
 #include <geekos/pfat.h>
 #include <geekos/malloc.h>
 #include <geekos/string.h>
-#include <geekos/user.h>
 #include <geekos/elf.h>
 
 
@@ -28,9 +27,74 @@
  *   and entry address; to be filled in
  * @return 0 if successful, < 0 on error
  */
-int Parse_ELF_Executable(char *exeFileData, ulong_t exeFileLength,
-    struct Exe_Format *exeFormat)
+int Parse_ELF_Executable(char *exeFileData,
+			 ulong_t exeFileLength,
+			 struct Exe_Format *exeFormat)
 {
-    TODO("Parse an ELF executable image");
-}
+  /* Simple sanity check */
+  if (exeFileData == NULL || exeFileLength <= 0 || exeFormat == NULL)
+      return -1;
 
+  elfHeader *elf = (elfHeader *) exeFileData;
+
+  /* Check for magic number */
+  if (elf->ident[0] != 0x7f || elf->ident[1] != 'E' || 
+          elf->ident[2] != 'L' || elf->ident[3] != 'F') 
+      return -1;
+
+  /* Fail if not executable */
+  if (elf->type != 0x2)
+      return -1;
+
+  /* Fail if not supported version */
+  if (elf->version != 0x1)
+      return -1;
+
+  /* Fail if no valid program headers */
+  if (elf->phnum < 0)
+      return -1;
+
+  /* Fail if not supported architecture (Intel) */
+  if (elf->machine != 0x3)
+      return -1;
+
+  /* Fail if memory address are not "inside" the ELF */
+  if (elf->entry > exeFileLength || elf->phoff > exeFileLength ||
+          elf->sphoff > exeFileLength)
+      return -1;
+
+  /* Fail if reported ELF header size does not match actual
+   * ELF header size */
+  if (elf->ehsize != sizeof(elfHeader))
+      return -1;
+
+  /* Fail if reported program header size does not match actual
+   * program header size */
+  if (elf->phentsize != sizeof(programHeader))
+      return -1;
+
+  exeFormat->entryAddr = elf->entry;
+  exeFormat->numSegments = elf->phnum;
+  programHeader *program =
+      (programHeader *) (exeFileData + elf->phoff);
+
+  int i;
+  for (i = 0; i < elf->phnum; i++) {
+
+    /* Fail if memory address are not "inside" the ELF */
+    if (program->offset > exeFileLength ||
+            program->fileSize > exeFileLength ||
+            program->memSize > exeFileLength)
+        return -1;
+
+    exeFormat->segmentList[i].offsetInFile = program->offset;
+    exeFormat->segmentList[i].lengthInFile = program->fileSize;
+    exeFormat->segmentList[i].startAddress = program->vaddr;
+    exeFormat->segmentList[i].sizeInMemory = program->memSize;
+    exeFormat->segmentList[i].protFlags = program->flags;
+    program++;
+
+  }
+
+  return 0;
+}
